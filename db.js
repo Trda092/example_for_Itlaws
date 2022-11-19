276918412affc8ae76c53827930334b9dbf09ae8
Trda Ampornprasert <73659917+Trda092@users.noreply.github.com>
2022-11-19 17:02:13 +0700

    Add files via upload

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2')
 const db = mysql.createConnection({
-    user:"root",
-    host:"localhost",
-    password:"Ohmye123+++",
-    database:"prattler"
+    user:"u350327849_itlaw",
+    host:"31.220.110.101",
+    password:"!Itlaw1234",
+    database:"u350327849_project_itlaw"
 })
 
 db.connect(function(error){
